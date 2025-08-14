2bbb83f8da01ca285e24ab2d4f729bde30a405ad
Samuel Renaud <2392263@csfoy.ca>
2025-08-14 15:40:56 -0400

    Add steps decorator and registry

---
+++ goldenpipeline/registry.py
@@ -0,0 +1,22 @@
+from typing import Callable, TypeVar, Any
+
+T = TypeVar("T", bound=Callable[..., Any])
+
+STEP_REGISTRY: dict[str, Callable[..., Any]] = {}
+
+
+def register_step(name: str) -> Callable[[T], T]:
+    """
+Decorator that registers a function in STEP_REGISTRY under the given name.
+"""
+
+    def decorator(func: T) -> T:
+        try:
+            _ = STEP_REGISTRY[name]
+            raise ValueError(f"step with name: {name} already exists")
+        except KeyError:
+            STEP_REGISTRY[name] = func
+
+        return func
+
+    return decorator
