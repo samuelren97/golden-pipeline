42713974cbc9cc1b1c5254cd431af0d099250083
samuelren97 <122801020+samuelren97@users.noreply.github.com>
2025-08-14 17:10:12 -0400

    Add pipeline executor [#4] (#18)

--- goldenpipeline/main.py
+++
@@ -1,8 +0,0 @@
-import loader
-from registry import STEP_REGISTRY
-
-if __name__ == "__main__":
-    pipeline = loader.load_pipeline("pipeline.yaml")
-    print(pipeline)
-    print(STEP_REGISTRY)
-    STEP_REGISTRY["checkout"]()
---
+++ tests/test_executor.py
@@ -0,0 +1,32 @@
+from idlelib.config import InvalidConfigType
+
+import pytest
+
+from goldenpipeline.executor import execute_pipeline
+from goldenpipeline.loader import load_pipeline
+from goldenpipeline.registry import register_step, STEP_REGISTRY
+from tests.pipeline_content_test_cases import pipeline_content_unknown_step, pipeline_content_valid_config
+
+
+def test_valid_steps(tmp_path):
+    @register_step("checkout")
+    def checkout_step(params):
+        pass
+
+    pipeline_path = tmp_path / "pipeline.yaml"
+    pipeline_path.write_text(pipeline_content_valid_config)
+    pipeline = load_pipeline(str(pipeline_path))
+
+    try:
+        execute_pipeline(pipeline["steps"])
+    finally:
+        STEP_REGISTRY.clear()
+
+
+def test_unknown_step_invalid_config_type(tmp_path):
+    pipeline_path = tmp_path / "pipeline.yaml"
+    pipeline_path.write_text(pipeline_content_unknown_step)
+    pipeline = load_pipeline(str(pipeline_path))
+
+    with pytest.raises(InvalidConfigType):
+        execute_pipeline(pipeline["steps"])
