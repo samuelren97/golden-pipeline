150715678e85b32765763100ce6890c38982416b
Samuel Renaud <2392263@csfoy.ca>
2025-08-23 19:32:07 -0400

    Fix arg config in executor test

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -4,13 +4,15 @@ import pkgutil
 import pytest
 
 from goldenpipeline import steps
-from goldenpipeline.executor import execute_pipeline
 from goldenpipeline.InvalidConfigError import InvalidConfigError
+from goldenpipeline.executor import execute_pipeline
 from goldenpipeline.loader import load_pipeline
 from goldenpipeline.registry import STEP_REGISTRY
 from tests.args import init_args
-from tests.pipeline_content_test_cases import (pipeline_content_unknown_step,
-                                               pipeline_content_valid_config)
+from tests.pipeline_content_test_cases import (
+    pipeline_content_unknown_step,
+    pipeline_content_valid_config,
+)
 
 for _, module_name, _ in pkgutil.iter_modules(steps.__path__):
     importlib.import_module(f"goldenpipeline.steps.{module_name}")
@@ -35,6 +37,7 @@ def test_valid_steps(tmp_path):
 def test_unknown_step_invalid_config_type(tmp_path):
     args = init_args()
     pipeline_path = tmp_path / "pipeline.yaml"
+    args.config = str(pipeline_path)
     pipeline_path.write_text(pipeline_content_unknown_step)
     pipeline = load_pipeline(args)
 
