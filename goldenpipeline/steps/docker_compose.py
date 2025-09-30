4cba39fcb0384175c49257d19a0b17b62dc0fe10
Samuel Renaud <2392263@csfoy.ca>
2025-09-30 08:05:39 -0400

    added docker compose step

---
+++ goldenpipeline/steps/docker_compose.py
@@ -0,0 +1,48 @@
+import os
+import subprocess
+
+from goldenpipeline.logger import debug, info
+from goldenpipeline.registry import register_step
+from goldenpipeline.steps.utils import validate_step_required_params
+
+
+@register_step("docker-compose")
+def docker_compose_step(
+        params: dict,
+        is_verbose: bool,
+        is_dry_run: bool,
+) -> None:
+    required_params = [
+        "file",
+        "build",
+    ]
+
+    params_list = list(params.keys())
+    n_params = params
+
+    if "build" not in params_list:
+        n_params["build"] = False
+
+    if is_verbose:
+        debug("Validating pipeline copy parameters...")
+    validate_step_required_params(params_list, required_params)
+
+    info("Running docker compose...")
+    if not is_dry_run:
+        command = [
+            "docker",
+            "compose",
+            "-f",
+            n_params["file"],
+            "up",
+            "-d"
+        ]
+
+        if n_params["build"]:
+            command.append("--build")
+
+        subprocess.run(
+            command,
+            shell=os.name == "nt",
+            cwd=".",
+        )
